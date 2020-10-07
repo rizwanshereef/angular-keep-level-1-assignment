79e80a952bfce9d7e72b3e4e99c082262c8b2faa
rsrizwan77 <rsrizwan77@gmail.com>
2020-10-07 17:04:07 +0530

    Adding program

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import { NotesService } from './notes.service';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
-  declarations: [ ],
-  imports: [ ],
-  providers: [ ],
-  bootstrap: [ ]
+  declarations: [ AppComponent, HeaderComponent],
+  imports: [BrowserModule, BrowserAnimationsModule,HttpClientModule,MatToolbarModule,MatExpansionModule,
+    FormsModule,MatCardModule,MatButtonModule,MatInputModule,MatFormFieldModule ],
+  providers: [ NotesService],
+  bootstrap: [ AppComponent]
 })
 export class AppModule { }
